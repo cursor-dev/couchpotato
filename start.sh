79f835d6ee4dde1190dbbc8157126626fd92a541
Ryan Pederson <ryan@pederson.ca>
2016-02-04 08:58:51 -0700

    oops on su command

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ mkdir -p /volumes/downloads
 cd /opt/couchpotato
 touch /volumes/config/CouchPotato.cfg
 
-su - media -c exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log
+su - media -c 'exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log'
