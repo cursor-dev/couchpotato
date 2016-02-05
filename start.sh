beba59e8e85d7dc846194c354e46a922fea69b51
Ryan Pederson <ryan@pederson.ca>
2016-02-05 08:57:17 -0700

    rolled back the starting of couchpotato as the media user.

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ touch /volumes/config/CouchPotato.cfg
 chown -R root:media /volumes/config
 chmod -R 0755 /volumes/config
 
-su - media -c 'exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log'
+exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log
