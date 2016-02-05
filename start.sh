6115a2faf4cd672b1b60186b7b2987a96f596a0e
Ryan Pederson <ryan@pederson.ca>
2016-02-05 09:48:14 -0700

    redid the su - media -c "command" to start couchpotato. Derp.

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ mkdir -p /volumes/downloads
 touch /volumes/config/CouchPotato.cfg
 
 chown -R root:media /volumes/config
-chmod -R 0755 /volumes/config
+chmod -R 0770 /volumes/config
 
-exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log
+su - media -c 'exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log'
