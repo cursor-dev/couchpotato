dd4af476b0c11f4f0dda74f90458b529f5cc4a29
Ryan Pederson <ryan@pederson.ca>
2016-02-05 07:17:29 -0700

    Updated start.sh to first create the directories, then the config file, then assign permissions the files.

--- start.sh
+++ start.sh
@@ -3,10 +3,9 @@
 mkdir -p /volumes/config
 mkdir -p /volumes/media
 mkdir -p /volumes/downloads
+touch /volumes/config/CouchPotato.cfg
 
 chown -R root:media /volumes/config
-
-cd /opt/couchpotato
-touch /volumes/config/CouchPotato.cfg
+chmod -R 0755 /volumes/config
 
 su - media -c 'exec /usr/bin/python /opt/couchpotato/CouchPotato.py --data_dir /volumes/config/ --config_file=/volumes/config/CouchPotato.cfg --console_log'
